6a72d7e98dcbdef89163ba375cdb07899cf8fb83
matias sepulveda torres <matias.seto@gmail.com>
2020-08-24 19:04:44 -0400

    CalendarApp 1.1

---
+++ src/types/types.js
@@ -0,0 +1,24 @@
+
+export const types = {
+    uiOpenModal: '[ui] Open modal',
+    uiCloseModal: '[ui] Close modal',
+
+    eventAddNew: '[event] Add new',
+    eventLogout: '[event] Logout event',
+
+    eventStartAddNew: '[event] Start add new',
+    eventSetActive: '[event] Set active',
+    eventClearActiveEvent: '[event] Clear active event',
+    eventUpdated: '[event] Event updated',
+    eventDeleted: '[event] Event deleted',
+    eventLoaded: '[event] Event loaded',
+
+    authChecking: '[auth] Checking login state',
+    authCheckingFinich: '[auth] Finish Checking login state',
+    authStartLogin: '[auth] Start login',
+    authLogin: '[auth] Login',
+    authStartRegister: '[auth] Start Register',
+    authStartTokenRenew: '[auth] Start token renew',
+    authLogout: '[auth] Logout',
+    
+}
